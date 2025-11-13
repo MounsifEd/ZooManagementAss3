e0d185ed8c6fc76e6b7c8a014746379fb10819ab
Mounsif <wdadsa@fsdfdsa.com>
2025-11-12 20:15:15 -0500

    added final JavaDoc in EnclosureCollection

--- src/main/java/com/example/zoomanagementass3/Model/EnclosureCollection.java
+++ src/main/java/com/example/zoomanagementass3/Model/EnclosureCollection.java
@@ -1,5 +1,9 @@
 package com.example.zoomanagementass3.Model;
 
+/**
+ * Represents component in the structure.
+ * @Author Mounsif
+ */
 public interface EnclosureCollection {
     void addCollection(EnclosureCollection collection);
     String getName();
